bf3741310b9005fd0718e31cd7a60dc5c3379bc1
JHA <1004067303@qq.com>
2024-11-26 17:28:22 +0800

    单元测试模块添加，未完全添加

---
+++ JunitTest/src/Test/MyUtilsTest.java
@@ -0,0 +1,9 @@
+package Test;
+
+import Utils.MyUtils;
+public class MyUtilsTest {
+
+    public void testGetMaxIndex(){
+        System.out.println(MyUtils.getMaxIndex("芜湖起飞！"));
+    }
+}
---
+++ JunitTest/src/Utils/MyUtils.java
@@ -0,0 +1,17 @@
+package Utils;
+
+public class MyUtils {
+    public static int getMaxIndex(String msg){
+        int length=0;
+        if(msg==null||msg==""){
+            length= -1;
+        }
+        length=msg.length()-1;
+        return length;
+    }
+
+
+    public static void getLength(String msg){
+        System.out.println(msg+"的长度为："+msg.length());
+    }
+}
