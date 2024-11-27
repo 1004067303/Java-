2fd522393a021e684f6e69b3e514c9aaadb367e9
JHA <1004067303@qq.com>
2024-11-27 10:53:52 +0800

    单元测试模块添加

--- JunitTest/src/JunitUtils/MyUtils.java
+++ JunitTest/src/JunitUtils/MyUtils.java
@@ -1,12 +1,13 @@
-package Utils;
+package JunitUtils;
 
 public class MyUtils {
     public static int getMaxIndex(String msg){
         int length=0;
         if(msg==null||msg==""){
             length= -1;
-        }
+        }else{
         length=msg.length()-1;
+        }
         return length;
     }
 
--- JunitTest/src/Test/MyUtilsTest.java
+++ JunitTest/src/Test/MyUtilsTest.java
@@ -1,9 +1,43 @@
 package Test;
 
-import Utils.MyUtils;
-public class MyUtilsTest {
+import JunitUtils.MyUtils;
+import org.junit.*;
 
-    public void testGetMaxIndex(){
-        System.out.println(MyUtils.getMaxIndex("芜湖起飞！"));
+
+public class MyUtilsTest {
+    @BeforeClass//在Junit5之后变成BeforeALL,一般用来释放资源
+    public static void BeforeClass(){
+        System.out.println("BeforeClass----->只执行一次，在所有之前！！");
+    }
+    @Before//在Junit5之后变成BeforeEach
+    public void before(){
+        System.out.println("before------->执行！！！");
+    }
+    @org.junit.Test
+    public void getMaxIndex() {
+        int index1 = MyUtils.getMaxIndex("芜湖起飞！！");
+        System.out.println(index1);
+        System.out.println(MyUtils.getMaxIndex(""));
+        int index2 = MyUtils.getMaxIndex(null);
+        System.out.println(index2);
+        //断言机制
+        Assert.assertEquals("getMaxIndex方法存在问题",5,index1);
+
+    }
+
+    @org.junit.Test
+    public void getLength() {
+        MyUtils.getLength("芜湖起飞！！！");
+        MyUtils.getLength("");
+       // MyUtils.getLength(null);
+
+    }
+    @AfterClass//在Junit5之后变成AfterALL
+    public static void AfterClass(){
+        System.out.println("AfterClass----->只执行一次，在所有之后！！");
+    }
+    @After//在Junit5之后变成AfterEach
+    public void After(){
+        System.out.println("After------>执行");
     }
 }
